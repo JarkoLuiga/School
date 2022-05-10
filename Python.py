286fa7678f4cc2c29c5cbbeff0ff0e08260fc046
JarkoLuiga <102141842+JarkoLuiga@users.noreply.github.com>
2022-05-10 13:16:17 +0300

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,14 +1,22 @@
-str1 = int(open("file1", "r").read())
-str2 = int(open("file2", "r").read())
-if str1 + str2 < 20:
-    print(str1 + str2)
-else:
-    open("tulemus.txt","w").write("Hallo!" * 20)
+def konsooli_print(tulemus): # funktsiooni deklaratsioon
+    if tulemus < 20:
+        for _ in range(tulemus): # nii mitu korda kui on tulemus prindin tulemust
+            print("Tere!")
 
-str1 = int(open("A.txt", "r").read())
-str2 = int(open("B.txt", "r").read())
-s = str1 + str2
-if s > 20:
-    print(str1 + str2)
+file1 = open("A.txt", "r") # avan esimese faili file1 = muutuja, funktsioon = open("A.txt", "r")
+file2 = open("B.txt", "r") # avan teise faili
+muutuja1 = int(file1.read()) # loen faili read funktsiooni kasutades loen faili ning muudan selle intiks
+muutuja2 = int(file2.read()) # loen faili, see on argument (file2.read())
+tulemus = muutuja1 + muutuja2 # muutuja väärtustamine
+
+if tulemus > 1000000:
+    tulemusfile = open("miljon2r.txt", "w") # loon uue faili
 else:
-    open("tulemus.txt","w").write("Hallo!" * s)
+    tulemusfile = open("tulemus.txt", "w") # loon uue faili
+
+tulemusfile.write(str(tulemus)) # kirjutan tulemuse stringina, kirjutan selle write funktsiooni abil
+
+konsooli_print(tulemus) # kutsun funktsiooni 
+
+file1.close() # sulgen faili
+file2.close() # sulgen faili
